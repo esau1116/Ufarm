78c864b39704708cf52d42072c0ef01138f9f1c8
kaks <esaukaks@gmail.com>
2020-11-06 02:03:42 +0300

    changes made

--- public/js/uFarmReg.js
+++ public/js/uFarmReg.js
@@ -1,17 +1,26 @@
 const subform = () => {
-  const uName = document.registration.username;
-  const uDob = document.registration.dateOfBirth;
-  const uDor = document.registration.dateOfRegiration;
-  const uNinn = document.registration.nationalIdentificationNumber;
-  const uContact = document.registration.contact;
+  const uName = document.registration.firstName;
+  const uName2 = document.registration.surName;
+  //const uDob = document.registration.dateOfBirth;
+  //const uDor = document.registration.dateOfRegiration;
+  const uNinn = document.registration.nin;
+  const uContact = document.registration.phoneNumber;
+  const uWard = document.registration.ward;
+
   //Defining regex for name
   const alphanumeric = /^[0-9a-zA-Z]+$/;
 
   // Initializing if function for username
   if (uName.value.match(alphanumeric)) {
-    uName.style.border = "2px solid green";
+    uName.style.border = "4px solid blue";
   } else {
-    uName.style.border = "2px solid red";
+    uName.style.border = "4px solid red";
+  }
+  // Initializing if function for username
+  if (uName2.value.match(alphanumeric)) {
+    uName2.style.border = "4px solid blue";
+  } else {
+    uName2.style.border = "4px solid red";
   }
 
   // Defining regex for date
@@ -34,17 +43,23 @@ const subform = () => {
   //const alphanumeric = /^[0-9a-zA-Z]+$/;
   // Initializing if function for username
   if (uNinn.value.match(alphanumeric)) {
-    uNinn.style.border = "2px solid green";
+    uNinn.style.border = "4px solid blue";
   } else {
-    uNinn.style.border = "2px solid red";
+    uNinn.style.border = "4px solid red";
   }
   //definig regex for contact
   const contactRegex = /^\d{10}$/;
 
   //Initiallising the function of contact
   if (uContact.value.match(contactRegex)) {
-    uContact.style.border = "3px solid green";
+    uContact.style.border = "4px solid blue";
   } else {
-    uContact.style.border = "3px solid green";
+    uContact.style.border = "4px solid red";
+  }
+  //Initiallising function for ward
+  if (uWard.value.match(alphanumeric)) {
+    uWard.style.border = "4px solid blue";
+  } else {
+    uWard.style.border = "4px solid red";
   }
 };
